fb39eb1ecb2816e56c32fcb90679f439e213ee58
pthienquan1 <quan01011997123@gmail.com>
2023-05-25 16:13:06 +0700

    create IAccount & IDepartment Repository

---
+++ Review2-Springboot/4.Trans-Validate-Internatinalization-Swagger-SpringDataRest/src/main/java/review/repository/IAccountRepository.java
@@ -0,0 +1,14 @@
+package review.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import review.dto.DepartmentDTO;
+import review.entity.Account;
+
+/**
+ * Created by quan0
+ * Date 5/25/2023 - 4:10 PM
+ * Description: ...
+ */
+public interface IAccountRepository extends JpaRepository<Account, Integer>, JpaSpecificationExecutor<Account> {
+}
---
+++ Review2-Springboot/4.Trans-Validate-Internatinalization-Swagger-SpringDataRest/src/main/java/review/repository/IDepartmentRepository.java
@@ -0,0 +1,13 @@
+package review.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import review.entity.Department;
+
+/**
+ * Created by quan0
+ * Date 5/25/2023 - 4:10 PM
+ * Description: ...
+ */
+public interface IDepartmentRepository extends JpaRepository<Department, Integer>, JpaSpecificationExecutor<Department> {
+}
