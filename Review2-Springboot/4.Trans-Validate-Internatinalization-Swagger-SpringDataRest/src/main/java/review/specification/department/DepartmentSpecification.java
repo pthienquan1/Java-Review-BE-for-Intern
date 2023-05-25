e7f5eeaf4020816e44241cedf58563e0b168ac6a
pthienquan1 <quan01011997123@gmail.com>
2023-05-25 18:04:13 +0700

    Add DepartmentSpecification (include CustomSpecification)

---
+++ Review2-Springboot/4.Trans-Validate-Internatinalization-Swagger-SpringDataRest/src/main/java/review/specification/department/DepartmentSpecification.java
@@ -0,0 +1,123 @@
+package review.specification.department;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+import review.entity.Department;
+import review.form.DepartmentFilterForm;
+
+import javax.persistence.criteria.*;
+import java.util.Date;
+
+/**
+ * Created by quan0
+ * Date 5/25/2023 - 5:31 PM
+ * Description: ...
+ */
+public class DepartmentSpecification {
+    @SuppressWarnings("deprecation")
+    private static Specification<Department> buildWhere(String search, DepartmentFilterForm filterForm){
+        Specification<Department> where = null;
+        if(!StringUtils.isEmpty(search) ){
+            search = search.trim();
+            CustomSpecification username = new CustomSpecification("username",search);
+            where = Specification.where(username);
+        }
+        if(filterForm != null && filterForm.getCreatedDate() != null){
+            CustomSpecification createdDate = new CustomSpecification("createdDate", filterForm.getCreatedDate());
+            if(where == null){
+                where = createdDate;
+            }
+            else{
+                where= where.and(createdDate);
+            }
+        }
+        if(filterForm != null && filterForm.getMinCreatedDate() != null){
+            CustomSpecification minCreatedDate = new CustomSpecification("minCreatedDate",filterForm.getMinCreatedDate());
+            if(where == null){
+                where = minCreatedDate;
+            }
+            else{
+                where= where.and(minCreatedDate);
+            }
+        }
+        if (filterForm != null && filterForm.getMaxCreatedDate() != null) {
+            CustomSpecification maxCreatedDate = new CustomSpecification("maxCreatedDate", filterForm.getMaxCreatedDate());
+            if (where == null) {
+                where = maxCreatedDate;
+            } else {
+                where = where.and(maxCreatedDate);
+            }
+        }
+
+
+        if (filterForm != null && filterForm.getMinYear() != null) {
+            CustomSpecification minYear = new CustomSpecification("minYear", filterForm.getMinYear());
+            if (where == null) {
+                where = minYear;
+            } else {
+                where = where.and(minYear);
+            }
+        }
+
+        if (filterForm != null && filterForm.getType() != null) {
+            CustomSpecification type = new CustomSpecification("type", filterForm.getType());
+            if (where == null) {
+                where = type;
+            } else {
+                where = where.and(type);
+            }
+        }
+
+        return where;
+    }
+    }
+
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@SuppressWarnings("serial")
+class CustomSpecification implements Specification<Department> {
+    @NonNull
+    private String field;
+    @NonNull
+    private Object value;
+
+    @Override
+    public Predicate toPredicate(Root<Department> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+
+        if(field.equalsIgnoreCase("username")){
+            Join join = root.join("accounts", JoinType.LEFT);
+            return criteriaBuilder.like(join.get("username"), "%" + value.toString() + "%");
+        }
+        if(field.equalsIgnoreCase("createdDate")){
+            return criteriaBuilder.equal(root.get("createdDate").as(java.sql.Date.class),(Date) value);
+        }
+        if (field.equalsIgnoreCase("minCreatedDate")) {
+            return criteriaBuilder.greaterThanOrEqualTo(
+                    root.get("createdDate").as(java.sql.Date.class),
+                    (Date) value);
+        }
+
+        if (field.equalsIgnoreCase("maxCreatedDate")) {
+            return criteriaBuilder.lessThanOrEqualTo(
+                    root.get("createdDate").as(java.sql.Date.class),
+                    (Date) value);
+        }
+
+        if (field.equalsIgnoreCase("minYear")) {
+            return criteriaBuilder.greaterThanOrEqualTo(
+                    criteriaBuilder.function("YEAR", Integer.class, root.get("createdDate")),
+                    (Integer) value);
+        }
+
+        if (field.equalsIgnoreCase("type")) {
+            return criteriaBuilder.equal(root.get("type"), value);
+        }
+
+        return null;
+    }
+}
